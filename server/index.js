8b3062ef14330fff977fc252b0516b5bbc53794b
Steven Croft <scroft001@gmail.com>
2022-10-17 21:58:53 -0700

    graphiql must be set to true in app.use

--- server/index.js
+++ server/index.js
@@ -10,6 +10,7 @@ const app = express();
 app.use('/graphql', graphqlHTTP({
     schema,
     graphiql: process.env.NODE_ENV === 'development',
+    graphiql: true,
 }))
 
 app.listen(port, console.log("Server listening on port " + port));
